68a54b197d5d7f10bb357f6ce5f477d8ae05a7f5
botdahl <botdahlstrom@gmail.com>
2024-11-29 12:50:07 +0100

    feat: part2 done

--- OlleDahlstrom/tit_tac_toe_game_2.cpp
+++ OlleDahlstrom/tit_tac_toe_game_2.cpp
@@ -1,124 +1,33 @@
 #include <iostream>
 #include <stdlib.h>
 
-void displayBoard(char board[3][3]);
-int getPlayerMove(char board[3][3]);
-void setPlayerMark(int coordinate, char playerMark, char board[3][3]);
-bool checkWin(char board[3][3]);
-bool checkDraw(char board[3][3]);
-
-int main()
+class Game
 {
-    // empty 2d matrix
-    char board[3][3] = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+    private:
+        char board[3][3] = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+    public:
+        void displayBoard();
+        bool checkWin();
+        bool checkDraw();
+        void setMark(int coordinate, char playerMark);
+        bool isEmpty(int coordinate);
+};
 
-    // define players
-    char playerX = 'X';
-    char playerO = 'O';
-
-    bool gameRunning = true;
-
-    std::cout << "Welcome to Tic-Tac-Toe!\n";
-    displayBoard(board);
-
-    // main loop
-    while (gameRunning) {
-        std::cout << playerX << "'s turn.\n";
-        setPlayerMark(getPlayerMove(board), playerX, board);
-        displayBoard(board);
-        if (checkWin(board)) {
-            std::cout << "Winner: " << playerX << std::endl;
-            break;
-        }
-        if (checkDraw(board)) {
-            std::cout << "Draw!" << std::endl;
-            break;
-        }
-
-        std::cout << playerO << "'s turn.\n";
-        setPlayerMark(getPlayerMove(board), playerO, board);
-        displayBoard(board);
-        if (checkWin(board)) {
-            std::cout << "Winner: " << playerO << std::endl;
-            break;
-        }
-        if (checkDraw(board)) {
-            std::cout << "Draw!" << std::endl;
-            break;
-        }
-    }
-
-    return 0;
-}
-
-void displayBoard(char board[3][3])
+void Game::displayBoard()
 {
     system("clear");
-    std::cout << "+---+---+---+" << std::endl;
+    std::cout << "+---+---+---+\n";
     for (int row = 0; row < 3; row++) {
-        std::cout << "| ";
+        std::cout << "|";
         for (int col = 0; col < 3; col++) {
-            std::cout << board[row][col] << " | ";
+            std::cout << " " << this->board[row][col] << " |";
         }
-        std::cout << std::endl;
-        std::cout << "+---+---+---+" << std::endl;
+        std::cout << "\n";
+        std::cout << "+---+---+---+\n";
     }
 }
 
-int getPlayerMove(char board[3][3])
-{
-    int coordinate;
-    
-    while (1) {
-        std::cout << "Enter coordinate for your move (1-9): ";
-        std::cin >> coordinate;
-        if (coordinate > 0 && coordinate < 10) {
-
-            int row;
-            int col;
-
-            // get row and col
-            if (coordinate < 4) {
-                row = 0;
-                col = (coordinate - 1);
-            } else if (coordinate < 7) {
-                row = 1;
-                col = (coordinate - 1) % 3;
-            } else if (coordinate < 10) {
-                row = 2;
-                col = (coordinate - 1) % 6;
-            }
-
-            if (board[row][col] == ' ') {
-                return coordinate;
-            }
-            std::cout << "Cell not empty, choose another one." << std::endl;
-        } else {
-            std::cout << "Out of range, choose a valid range." << std::endl;
-        }
-    }
-}
-
-void setPlayerMark(int coordinate, char playerMark, char board[3][3])
-{
-    int row;
-    int col;
-
-    if (coordinate < 4) {
-        row = 0;
-        col = (coordinate - 1);
-    } else if (coordinate < 7) {
-        row = 1;
-        col = (coordinate - 1) % 3;
-    } else if (coordinate < 10) {
-        row = 2;
-        col = (coordinate - 1) % 6;
-    }
-
-    board[row][col] = playerMark;
-}
-
-bool checkWin(char board[3][3])
+bool Game::checkWin()
 {
     for (int i = 0; i < 3; i++) {
         if ((board[i][0] != ' ' &&
@@ -146,15 +55,134 @@ bool checkWin(char board[3][3])
     return false;
 }
 
-bool checkDraw(char board[3][3])
+bool Game::checkDraw()
 {
     for (int row = 0; row < 3; row++) {
         for (int col = 0; col < 3; col++) {
-            if (board[row][col] != ' ') {
+            if (this->board[row][col] != ' ') {
                 return false;
             }
         }
     }
 
     return true;
+}
+
+void Game::setMark(int coordinate, char playerMark)
+{
+    int row;
+    int col;
+
+    if (coordinate < 4) {
+        row = 0;
+        col = (coordinate - 1);
+    } else if (coordinate < 7) {
+        row = 1;
+        col = (coordinate - 1) % 3;
+    } else {
+        row = 2;
+        col = (coordinate - 1) % 6;
+    }
+
+    this->board[row][col] = playerMark;
+}
+
+bool Game::isEmpty(int coordinate) {
+    int row;
+    int col;
+
+    if (coordinate < 4) {
+        row = 0;
+        col = (coordinate - 1);
+    } else if (coordinate < 7) {
+        row = 1;
+        col = (coordinate - 1) % 3;
+    } else {
+        row = 2;
+        col = (coordinate - 1) % 6;
+    }
+
+    if (this->board[row][col] == ' ') {
+        return true;
+    }
+    return false;
+}
+
+class Player
+{
+    private:
+        char playerMark;
+    public:
+        Player(char playerMark) : playerMark(playerMark) {}
+        char getPlayerMark();
+        int getMove(Game game);
+};
+
+char Player::getPlayerMark()
+{
+    return this->playerMark;
+}
+
+int Player::getMove(Game game)
+{
+    int coordinate;
+
+    while (1) {
+        std::cout << "Enter coordinate for your move: (1-9): ";
+        std::cin >> coordinate;
+
+        if (coordinate > 0 && coordinate < 10) {
+
+            if (game.isEmpty(coordinate)) {
+                return coordinate;
+            }
+
+            std::cout << "Cell not empty, choose another one.\n";
+        } else {
+            std::cout << "Out of range, choose a valid range.\n";
+        }
+    }
+}
+
+int main()
+{
+    Game game;
+    Player playerX('X');
+    Player playerO('O');
+
+    bool gameRunning = true;
+
+    std::cout << "Welcome to Tic-Tac-Toe!\n";
+    game.displayBoard();
+
+    while (gameRunning) {
+        std::cout << playerX.getPlayerMark() << "'s turn.\n";
+        game.setMark(playerX.getMove(game), playerX.getPlayerMark());
+        game.displayBoard();
+
+        if (game.checkWin()) {
+            std::cout << "Winner: " << playerX.getPlayerMark() << std::endl;
+            break;
+        }
+        if (game.checkDraw()) {
+            std::cout << "Draw!" << std::endl;
+            break;
+        }
+
+        std::cout << playerO.getPlayerMark() << "'s turn.\n";
+        game.setMark(playerO.getMove(game), playerO.getPlayerMark());
+        game.displayBoard();
+
+        if (game.checkWin()) {
+            std::cout << "Winner: " << playerO.getPlayerMark() << std::endl;
+            break;
+        }
+        if (game.checkDraw()) {
+            std::cout << "Draw!" << std::endl;
+            break;
+        }
+
+    }
+
+    return 0;
 }
